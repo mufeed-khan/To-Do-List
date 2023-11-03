f8415d1148827e1b47bed3ca5d4c1d47c4644843
Mufeed Khan <mufeedkhan192@gmail.com>
2023-11-03 21:36:02 +0530

    initial commit

---
+++ scrypt.js
@@ -0,0 +1,47 @@
+const inputBox = document.getElementById("input-box")
+
+const listContainer = document.getElementById("list-container")
+
+function addTask() {
+    if (inputBox.value === "") {
+        document.querySelector(".error").style.display = "block"
+
+    }
+    else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+
+        li.appendChild(span)
+        document.querySelector(".error").style.display = "none"
+    }
+    inputBox.value = '';
+    saveData()
+
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+        saveData()
+
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+
+    }
+
+}, false)
+
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask()
